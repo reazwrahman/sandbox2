7c3f20f0a9df9bc306a0a58da69eb11c407d32f0
Reaz <reaz.rahman@macaulay.cuny.edu>
2022-01-08 10:34:13 -0500

    trying out stuff

--- fantasy_squad.py
+++ fantasy_squad.py
@@ -1,6 +1,6 @@
 import os
 import click
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from app import create_app, db
 from app.models import User, Role, GameDetails
 
@@ -24,6 +24,17 @@ def test(test_names):
         tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
 
-if __name__=="__main__": 
-    app.run(debug=True)
+    # create or update user roles
+    #Role.insert_roles()
+
+    # ensure all users are following themselves
+    #User.add_self_follows()
+
+#if __name__=="__main__": 
+#    app.run(debug=True)
